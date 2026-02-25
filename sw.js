4fe2637041088d2c3ed9b48fa75f174051b126cc
mickar1987 <mickar1987@gmail.com>
2026-02-25 21:56:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'shithead-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys =>
+        Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    // Network first for socket.io, cache first for assets
+    if (e.request.url.includes('socket.io')) return;
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
+});
